1255b625e134a4efcf1f39206b2c7335ced01e72
matthias.bisping <matthias.bisping@17e6b784-d33f-46b6-9e08-c096d7d620ad>
2017-09-27 08:11:04 +0000

    remove formatter from tests

--- test/basic/PrideForUpdateTest.java
+++ test/basic/PrideForUpdateTest.java
@@ -1,16 +1,13 @@
 package basic;
 
-import de.mathema.pride.DatabaseFactory;
-import de.mathema.pride.SQLFormatter;
 import de.mathema.pride.WhereCondition;
 import org.junit.Test;
 
 import java.sql.SQLException;
 
-import static de.mathema.pride.WhereCondition.Operator.EQUAL;
 import static de.mathema.pride.WhereCondition.Operator.UNEQUAL;
 
-public class PrideForUpdateTest extends AbstractPrideTest implements SQLFormatter {
+public class PrideForUpdateTest extends AbstractPrideTest{
 
     @Override
     public void setUp() throws Exception {
@@ -24,28 +21,7 @@ public class PrideForUpdateTest extends AbstractPrideTest implements SQLFormatte
                 .and("firstName", UNEQUAL, null)
                 .forUpdate();
 
-        assertEquals("( firstName IS NOT NULL ) FOR UPDATE", expression.toSQL(this));
+        assertEquals("( firstName IS NOT null ) FOR UPDATE", expression.toString());
         assertNotNull(new Customer().query(expression));
-    }
-
-    @Override
-    public String formatValue(Object rawValue) {
-        return DatabaseFactory.getDatabase().formatValue(rawValue);
-    }
-
-    @Override
-    public String formatOperator(String operator, Object rawValue) {
-        if (operator.equals(EQUAL)) {
-            return (rawValue == null) ? "IS" : operator;
-        }
-        if (operator.equals(UNEQUAL)) {
-            return (rawValue == null) ? "IS NOT" : operator;
-        }
-        return operator;
-    }
-
-    @Override
-    public Object formatPreparedValue(Object rawValue) {
-        return DatabaseFactory.getDatabase().formatPreparedValue(rawValue);
     }
 }

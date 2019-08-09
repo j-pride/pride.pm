7fdb4a3917a67b2e738c3aac8b9321037c33a6c6
Florian Schlag <git@florianschlag.de>
2019-08-09 12:38:00 +0200

    PostgresSQL doesn't have a CLOB type either

--- src/test/java/basic/ClobBlobTest.java
+++ src/test/java/basic/ClobBlobTest.java
@@ -8,8 +8,12 @@ import pm.pride.ResourceAccessor;
 
 @SkipForDBType(value = {
 		ResourceAccessor.DBType.SQLITE,
-		/* TODO JL: MariaDB doesn't have a CLOB type, try to use LONGTEXT or something else instead */
-		ResourceAccessor.DBType.MARIADB
+		/* TODO JL: MariaDB and Postgres doesn't have a CLOB type, try to use LONGTEXT/text or something else instead
+		 * For PostgreSQL see: https://stackoverflow.com/a/49964770/8745384
+		 * For MariaDB see: https://mariadb.com/kb/en/library/blob-and-text-data-types/
+		 * */
+		ResourceAccessor.DBType.MARIADB,
+		ResourceAccessor.DBType.POSTGRES
 })
 public class ClobBlobTest extends AbstractPrideTest {
     protected void createClobBlobTable() throws SQLException {

926c4bb07c8c9ca8f1edfedac5f9f4f1c1c3f52c
jlessner <jlessner@gmx.de>
2020-03-06 22:16:25 +0100

    format

--- src/main/java/pm/pride/util/generator/TableColumn.java
+++ src/main/java/pm/pride/util/generator/TableColumn.java
@@ -13,144 +13,154 @@ import pm.pride.ResourceAccessor;
 public class TableColumn {
 
 	protected String dbType;
-    protected String tableName;
-    protected String columnName;
-    protected String uniqueColumnName;
-    protected int columnType;
-    protected int columnSize;
-    protected int decimalDigits;
-    protected boolean nullsForbidden;
-    protected boolean primaryKeyField;
-    
-    public TableColumn(String dbType, String tableName, String columnName, int type, int columnSize, int decimalDigits, boolean nullsForbidden) {
-    	this.dbType = dbType;
-    	this.tableName = tableName;
-        this.columnName = columnName;
-        this.uniqueColumnName = columnName;
-        this.columnType = type;
-        this.columnSize = columnSize;
-        this.decimalDigits = decimalDigits;
-        this.nullsForbidden = nullsForbidden;
-    }
+	protected String tableName;
+	protected String columnName;
+	protected String uniqueColumnName;
+	protected int columnType;
+	protected int columnSize;
+	protected int decimalDigits;
+	protected boolean nullsForbidden;
+	protected boolean primaryKeyField;
 
-	public void setPrimaryKeyField() { primaryKeyField = true; }
+	public TableColumn(String dbType, String tableName, String columnName, int type, int columnSize, int decimalDigits,
+			boolean nullsForbidden) {
+		this.dbType = dbType;
+		this.tableName = tableName;
+		this.columnName = columnName;
+		this.uniqueColumnName = columnName;
+		this.columnType = type;
+		this.columnSize = columnSize;
+		this.decimalDigits = decimalDigits;
+		this.nullsForbidden = nullsForbidden;
+	}
+
+	public void setPrimaryKeyField() {
+		primaryKeyField = true;
+	}
 
 	public void makeUnique() {
 		this.uniqueColumnName = this.tableName + "_" + this.columnName;
 	}
 
 	/** @return true if this column is a primary key field */
-	public boolean isPrimaryKeyField() { return primaryKeyField; }
+	public boolean isPrimaryKeyField() {
+		return primaryKeyField;
+	}
 
-    /**
-     * @return column name in lower case and doesnt remove character underscore
-     */
-    public String getName() { return columnName.toLowerCase(); }
+	/**
+	 * @return column name in lower case and doesnt remove character underscore
+	 */
+	public String getName() {
+		return columnName.toLowerCase();
+	}
 
-    public String getUniqueName() { return uniqueColumnName.toLowerCase(); }
+	public String getUniqueName() {
+		return uniqueColumnName.toLowerCase();
+	}
 
-    /**
-     * @return capitalized column name without underscores. Characters following
-     * directly after underscores are printed in uppercase.
-     */
-    public String getNameCamelCaseFirstUp() {
-        StringBuffer newString = new StringBuffer();
-        boolean nextUpper = true;
-        for (int i=0; i < uniqueColumnName.length(); i++) {
-            if ( uniqueColumnName.substring(i,i + 1).equals("_") ) {
-                nextUpper = true;
-            }
-            else {
-                if ( nextUpper ) {
-                    newString.append(uniqueColumnName.substring(i,i + 1).toUpperCase());
-                    nextUpper = false;
-                }
-                else
-                    newString.append(uniqueColumnName.substring(i,i + 1).toLowerCase());
-            }
-        }
-        return newString.toString();
-    }
-    
-    /**
-     * @return column name. Like getName2 but prints the first letter in lowercase.
-     */
-    public String getNameCamelCaseFirstLow() {
-    	String firstUp = getNameCamelCaseFirstUp();
-    	String head = firstUp.substring(0, 1);
-    	return head.toLowerCase() + firstUp.substring(1);
-    }
-    
-    public String getNameUpper() {
-    	return getUniqueName().toUpperCase();
-    }
-    
-    /**
-     * @return String with JAVA type of the column. If the column doesn't allow
-     * null values, the functions returns primitive types, otherwise it returns
-     * object types.
-     */
-    public String getType() {
-        String returnType = null;
-        
-        switch (columnType) {
-        case Types.VARCHAR  :
-        case Types.NVARCHAR  :
-            returnType = "String";
-            break;
-        case Types.CHAR     :
-            returnType = "String";
-            break;
-        case Types.SMALLINT :
-        case Types.TINYINT  :
-            returnType = nullsForbidden ? "short" : "Short";
-            break;
-        case Types.NUMERIC  :
-        case Types.DECIMAL  :
-            if (decimalDigits > 0)
-                returnType = "java.math.BigDecimal";
-            else
-                returnType = nullsForbidden ? "long" : "Long";
-            break;
-        case Types.INTEGER  :
-            returnType = nullsForbidden ? "int" : "Integer";
-            break;
-        case Types.REAL     :
-            returnType = nullsForbidden ? "float" : "Float";
-            break;
-        case Types.FLOAT    :
-            returnType = nullsForbidden ? "double" : "Double";
-            break;
-        case Types.DOUBLE   :
-            returnType = nullsForbidden ? "double" : "Double";
-            break;
-        case Types.BIGINT   :
-            returnType = nullsForbidden ? "long" : "Long";
-            break;
-        case Types.DATE     :
-            returnType = "java.util.Date";
-            break;
-        case Types.BOOLEAN  :
-            returnType = nullsForbidden ? "boolean" : "Boolean";
-            break;
-        case Types.CLOB     :
-            returnType = "Clob";
-            break;
-        case Types.TIMESTAMP:
-            returnType = "java.sql.Timestamp";
-            /** Special issue with Oracle: DATE columns are reported to have type TIMESTAMP
-             * via the JDBC meta data. A time stamp with a short column size is a date.
-             */
-        	if (ResourceAccessor.DBType.ORACLE.equals(dbType) && columnSize < 11) {
-                returnType = "java.util.Date";
-        	}
-            break;
-        default:
-        	returnType = "Object";
-        }
-        
-        return returnType;
-    }
+	/**
+	 * @return capitalized column name without underscores. Characters following
+	 *         directly after underscores are printed in uppercase.
+	 */
+	public String getNameCamelCaseFirstUp() {
+		StringBuffer newString = new StringBuffer();
+		boolean nextUpper = true;
+		for (int i = 0; i < uniqueColumnName.length(); i++) {
+			if (uniqueColumnName.substring(i, i + 1).equals("_")) {
+				nextUpper = true;
+			} else {
+				if (nextUpper) {
+					newString.append(uniqueColumnName.substring(i, i + 1).toUpperCase());
+					nextUpper = false;
+				} else
+					newString.append(uniqueColumnName.substring(i, i + 1).toLowerCase());
+			}
+		}
+		return newString.toString();
+	}
+
+	/**
+	 * @return column name. Like getName2 but prints the first letter in
+	 *         lowercase.
+	 */
+	public String getNameCamelCaseFirstLow() {
+		String firstUp = getNameCamelCaseFirstUp();
+		String head = firstUp.substring(0, 1);
+		return head.toLowerCase() + firstUp.substring(1);
+	}
+
+	public String getNameUpper() {
+		return getUniqueName().toUpperCase();
+	}
+
+	/**
+	 * @return String with JAVA type of the column. If the column doesn't allow
+	 *         null values, the functions returns primitive types, otherwise it
+	 *         returns object types.
+	 */
+	public String getType() {
+		String returnType = null;
+
+		switch (columnType) {
+		case Types.VARCHAR:
+		case Types.NVARCHAR:
+			returnType = "String";
+			break;
+		case Types.CHAR:
+			returnType = "String";
+			break;
+		case Types.SMALLINT:
+		case Types.TINYINT:
+			returnType = nullsForbidden ? "short" : "Short";
+			break;
+		case Types.NUMERIC:
+		case Types.DECIMAL:
+			if (decimalDigits > 0)
+				returnType = "java.math.BigDecimal";
+			else
+				returnType = nullsForbidden ? "long" : "Long";
+			break;
+		case Types.INTEGER:
+			returnType = nullsForbidden ? "int" : "Integer";
+			break;
+		case Types.REAL:
+			returnType = nullsForbidden ? "float" : "Float";
+			break;
+		case Types.FLOAT:
+			returnType = nullsForbidden ? "double" : "Double";
+			break;
+		case Types.DOUBLE:
+			returnType = nullsForbidden ? "double" : "Double";
+			break;
+		case Types.BIGINT:
+			returnType = nullsForbidden ? "long" : "Long";
+			break;
+		case Types.DATE:
+			returnType = "java.util.Date";
+			break;
+		case Types.BOOLEAN:
+			returnType = nullsForbidden ? "boolean" : "Boolean";
+			break;
+		case Types.CLOB:
+			returnType = "Clob";
+			break;
+		case Types.TIMESTAMP:
+			returnType = "java.sql.Timestamp";
+			/**
+			 * Special issue with Oracle: DATE columns are reported to have type
+			 * TIMESTAMP via the JDBC meta data. A time stamp with a short
+			 * column size is a date.
+			 */
+			if (ResourceAccessor.DBType.ORACLE.equals(dbType) && columnSize < 11) {
+				returnType = "java.util.Date";
+			}
+			break;
+		default:
+			returnType = "Object";
+		}
+
+		return returnType;
+	}
 
 	public String getTableName() {
 		return tableName;
--- src/main/java/pm/pride/util/generator/TableDescription.java
+++ src/main/java/pm/pride/util/generator/TableDescription.java
@@ -18,30 +18,31 @@ public class TableDescription {
 	public static final String COLUMN_LIST_START = "(";
 	public static final String COLUMN_LIST_END = ")";
 	public static final String COLUMN_LIST_SEPARATOR = ",";
-	
-    protected List<TableColumn> columnList;
-    protected String tableName;
-    protected boolean partial;
 
-	protected List<TableColumn> extractColumns(DatabaseMetaData db_meta, String dbType, String tableName, List<String> columnsOfInterest)
-			throws SQLException {
-		ResultSet rset2 = db_meta.getColumns (null, //con.getCatalog (),
-											  null, //"*",
-											  tableName, "%");
+	protected List<TableColumn> columnList;
+	protected String tableName;
+	protected boolean partial;
+
+	protected List<TableColumn> extractColumns(DatabaseMetaData db_meta, String dbType, String tableName,
+			List<String> columnsOfInterest) throws SQLException {
+		ResultSet rset2 = db_meta.getColumns(null, // con.getCatalog (),
+				null, // "*",
+				tableName, "%");
 		List<TableColumn> result = new ArrayList<>(0);
-		while (rset2.next ()) {
-			String columnName = rset2.getString ("COLUMN_NAME");
-			int dataType = rset2.getInt ("DATA_TYPE");
-			int columnSize = rset2.getInt ("COLUMN_SIZE"); // Timetamp: 11, Date: 7
-			int decimalDigits = rset2.getInt ("DECIMAL_DIGITS");
-			boolean nullable = rset2.getInt ("NULLABLE")== ResultSetMetaData.columnNoNulls;
+		while (rset2.next()) {
+			String columnName = rset2.getString("COLUMN_NAME");
+			int dataType = rset2.getInt("DATA_TYPE");
+			int columnSize = rset2.getInt("COLUMN_SIZE"); // Timetamp: 11, Date:
+															// 7
+			int decimalDigits = rset2.getInt("DECIMAL_DIGITS");
+			boolean nullable = rset2.getInt("NULLABLE") == ResultSetMetaData.columnNoNulls;
 			if (columnsOfInterest == null || columnsOfInterest.remove(columnName)) {
-				TableColumn tabColumn = new TableColumn(dbType, tableName, columnName,
-						dataType, columnSize, decimalDigits, nullable);
+				TableColumn tabColumn = new TableColumn(dbType, tableName, columnName, dataType, columnSize,
+						decimalDigits, nullable);
 				result.add(tabColumn);
 			}
 		}
-		rset2.close ();
+		rset2.close();
 
 		if (columnsOfInterest != null && columnsOfInterest.size() > 0) {
 			throw new SQLException("Unknown columns in table " + tableName + ": " + columnsOfInterest);
@@ -49,59 +50,59 @@ public class TableDescription {
 		return result;
 	}
 
-	protected void markKeyColumns(DatabaseMetaData db_meta, String tableName, List<TableColumn> columns) throws SQLException {
-		ResultSet rset2 = db_meta.getPrimaryKeys(null, //con.getCatalog (),
-								                 null, //"*",
-								                 tableName);
-		while (rset2.next ()) {
-			String column_name = rset2.getString ("COLUMN_NAME");
-	
+	protected void markKeyColumns(DatabaseMetaData db_meta, String tableName, List<TableColumn> columns)
+			throws SQLException {
+		ResultSet rset2 = db_meta.getPrimaryKeys(null, // con.getCatalog (),
+				null, // "*",
+				tableName);
+		while (rset2.next()) {
+			String column_name = rset2.getString("COLUMN_NAME");
+
 			Iterator<TableColumn> iter = columns.iterator();
-			while(iter.hasNext()) {
-			  TableColumn tabColumn = (TableColumn)iter.next();
-			  if (tabColumn.columnName.equals(column_name)) {
-				  tabColumn.setPrimaryKeyField();
-				  break;
-			  }
+			while (iter.hasNext()) {
+				TableColumn tabColumn = (TableColumn) iter.next();
+				if (tabColumn.columnName.equals(column_name)) {
+					tabColumn.setPrimaryKeyField();
+					break;
+				}
 			}
 		}
-		rset2.close ();
+		rset2.close();
 	}
 
 	protected void init(Connection con, String dbType, String tableName) throws SQLException {
 		if (tableName.contains(COLUMN_LIST_START)) {
 			this.tableName = extractColumnsFromTableName(con, dbType, tableName);
-		}
-		else {
+		} else {
 			this.tableName = extractColumnsFromTableMetadata(con, dbType, tableName, null);
 		}
 	}
-	
-    private String extractColumnsFromTableName(Connection con, String dbType, String tableName) throws SQLException {
-    	partial = true;
-    	String columnNamesString = tableName
-    			.replaceFirst(".+\\" + COLUMN_LIST_START, "")
-    			.replace(COLUMN_LIST_END, "")
-    			.replaceAll("\\s", "");
-    	String[] columnNames = columnNamesString.split(COLUMN_LIST_SEPARATOR);
-    	List<String> mutableColumnNameList = new ArrayList<>(Arrays.asList(columnNames));
-    	String cleanedTableName = tableName.replaceFirst("\\" + COLUMN_LIST_START + ".*\\" + COLUMN_LIST_END, "");
-    	return extractColumnsFromTableMetadata(con, dbType, cleanedTableName, mutableColumnNameList);
+
+	private String extractColumnsFromTableName(Connection con, String dbType, String tableName) throws SQLException {
+		partial = true;
+		String columnNamesString = tableName.replaceFirst(".+\\" + COLUMN_LIST_START, "").replace(COLUMN_LIST_END, "")
+				.replaceAll("\\s", "");
+		String[] columnNames = columnNamesString.split(COLUMN_LIST_SEPARATOR);
+		List<String> mutableColumnNameList = new ArrayList<>(Arrays.asList(columnNames));
+		String cleanedTableName = tableName.replaceFirst("\\" + COLUMN_LIST_START + ".*\\" + COLUMN_LIST_END, "");
+		return extractColumnsFromTableMetadata(con, dbType, cleanedTableName, mutableColumnNameList);
 	}
 
-	protected String extractColumnsFromTableMetadata(Connection con, String dbType, String tableName, List<String> columnsOfInterest) throws SQLException {
-		DatabaseMetaData db_meta = con.getMetaData ();
+	protected String extractColumnsFromTableMetadata(Connection con, String dbType, String tableName,
+			List<String> columnsOfInterest) throws SQLException {
+		DatabaseMetaData db_meta = con.getMetaData();
 		String[] tbl_types = { "TABLE", "VIEW" };
-		ResultSet rset1 = db_meta.getTables (null, //con.getCatalog (),
-											 null, //"*",
-											 tableName,
-											 tbl_types);
+		ResultSet rset1 = db_meta.getTables(null, // con.getCatalog (),
+				null, // "*",
+				tableName, tbl_types);
 		if (rset1.next()) {
-			tableName = rset1.getString ("TABLE_NAME"); // Just in case there is something to normalize ;-)
+			tableName = rset1.getString("TABLE_NAME"); // Just in case there is
+														// something to
+														// normalize ;-)
 			this.columnList = extractColumns(db_meta, dbType, tableName, columnsOfInterest);
 			markKeyColumns(db_meta, tableName, columnList);
 		}
-		rset1.close ();
+		rset1.close();
 		return tableName;
 	}
 
@@ -112,26 +113,33 @@ public class TableDescription {
 			if (columnList == null)
 				throw new SQLException("Unknown table " + tableName);
 		}
-    }
+	}
 
-    public boolean hasPrimaryKey() {
-        for (TableColumn current: getColumnList()) {
-            if (current.isPrimaryKeyField())
-                return true;
-        }
-        return false;
-    }
-    
-    /** Returns an enumerator for the table's columns in form of TableColumns objects */
-    public List<TableColumn> getColumnList() { return columnList; }
+	public boolean hasPrimaryKey() {
+		for (TableColumn current : getColumnList()) {
+			if (current.isPrimaryKeyField())
+				return true;
+		}
+		return false;
+	}
 
-    /** Returns the plain table name */
-    public String getTableName() { return tableName; }
+	/**
+	 * Returns an enumerator for the table's columns in form of TableColumns
+	 * objects
+	 */
+	public List<TableColumn> getColumnList() {
+		return columnList;
+	}
 
-    /** Returns the table name with its first letter capitalized */
-    public String getTableNameFirstUpper() {
-        return tableName.substring(0,1).toUpperCase() + tableName.substring(1);
-    }
+	/** Returns the plain table name */
+	public String getTableName() {
+		return tableName;
+	}
+
+	/** Returns the table name with its first letter capitalized */
+	public String getTableNameFirstUpper() {
+		return tableName.substring(0, 1).toUpperCase() + tableName.substring(1);
+	}
 
 	public boolean isPartial() {
 		return partial;

23ba680b3323af4369ca0a21965caccc910492d0
Florian Schlag <git@florianschlag.de>
2024-04-21 18:30:45 +0200

    Add ArchUnit meta tests

---
+++ src/test/java/pm/pride/EnsureCorrectTestSetupMetaTest.java
@@ -0,0 +1,34 @@
+package pm.pride;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import pm.pride.basic.AbstractPrideTest;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.methods;
+
+@AnalyzeClasses(packages = "pm.pride")
+public class EnsureCorrectTestSetupMetaTest {
+
+    @ArchTest
+    public static final ArchRule ensure_setUp_is_annotated_with_BeforeEach =
+            methods().that()
+                     .areDeclaredInClassesThat()
+                     .areAssignableTo(AbstractPrideTest.class)
+                     .and()
+                     .haveName("setUp")
+                     .should()
+                     .beAnnotatedWith(BeforeEach.class);
+
+    @ArchTest
+    public static final ArchRule ensure_tearDown_is_annotated_with_AfterEach =
+            methods().that()
+                     .areDeclaredInClassesThat()
+                     .areAssignableTo(AbstractPrideTest.class)
+                     .and()
+                     .haveName("tearDown")
+                     .should()
+                     .beAnnotatedWith(AfterEach.class);
+}

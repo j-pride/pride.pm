06c34d7c84fe074efdde3e38ae7c4255061789ff
lpieprzyk <luca.pieprzyk@sn-invent.de>
2025-10-19 15:20:12 +0200

    Enhanced SQL function tests by adding test for `upper` with additional query parameters and refactoring for reusable test data.

--- src/test/java/pm/pride/basic/SqlFunctionTest.java
+++ src/test/java/pm/pride/basic/SqlFunctionTest.java
@@ -7,15 +7,21 @@ import pm.pride.DatabaseFactory;
 import pm.pride.ResultIterator;
 import pm.pride.WhereCondition;
 
+import java.sql.Date;
 import java.sql.SQLException;
+import java.util.Collections;
+import java.util.GregorianCalendar;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static pm.pride.SqlFunction.*;
+import static pm.pride.WhereCondition.Operator.IN;
 
 class SqlFunctionTest extends AbstractPrideTest {
 
   public static final String TEST_CUSTOMER_LAST_NAME = "CuStOmER_NaMe";
+  public static final String TEST_CUSTOMER_FIRST_NAME = "FiRsT_NaMe";
+  public static final int TEST_CUSTOMER_ID = 1;
 
   @Override
   @BeforeEach
@@ -26,7 +32,7 @@ class SqlFunctionTest extends AbstractPrideTest {
   @Test
   void testFunction_upper() throws SQLException {
     Database db = DatabaseFactory.getDatabase();
-    Customer customer = new Customer(1);
+    Customer customer = new Customer(TEST_CUSTOMER_ID);
     customer.setLastName(TEST_CUSTOMER_LAST_NAME);
     customer.update();
     db.commit();
@@ -39,9 +45,30 @@ class SqlFunctionTest extends AbstractPrideTest {
   }
 
   @Test
+  void testFunction_upper_withOtherQueryParams() throws SQLException {
+    Database db = DatabaseFactory.getDatabase();
+    Customer customer = new Customer(TEST_CUSTOMER_ID);
+    customer.setLastName(TEST_CUSTOMER_LAST_NAME);
+    customer.setFirstName(TEST_CUSTOMER_FIRST_NAME);
+    Date myDate = new Date((new GregorianCalendar(1999, 12, 2)).getTimeInMillis());
+    customer.setHireDate(myDate);
+    customer.update();
+    db.commit();
+    WhereCondition wc = new WhereCondition(upper(Customer.COL_LASTNAME), upper(TEST_CUSTOMER_LAST_NAME.toLowerCase()))
+            .and(upper(Customer.COL_FIRSTNAME), upper(TEST_CUSTOMER_FIRST_NAME.toLowerCase()))
+            .and(Customer.COL_HIREDATE, myDate)
+            .and(Customer.COL_ID, IN, Collections.singletonList(TEST_CUSTOMER_ID).toArray());
+    try (ResultIterator resultIterator = customer.query(wc)) {
+      Customer customerInDb = resultIterator.getObject(Customer.class);
+      assertNotNull(customerInDb);
+      assertEquals(TEST_CUSTOMER_LAST_NAME, customerInDb.getLastName());
+    }
+  }
+
+  @Test
   void testFunction_lower() throws SQLException {
     Database db = DatabaseFactory.getDatabase();
-    Customer customer = new Customer(1);
+    Customer customer = new Customer(TEST_CUSTOMER_ID);
     customer.setLastName(TEST_CUSTOMER_LAST_NAME);
     customer.update();
     db.commit();
@@ -56,7 +83,7 @@ class SqlFunctionTest extends AbstractPrideTest {
   @Test
   void testFunction_trim() throws SQLException {
     Database db = DatabaseFactory.getDatabase();
-    Customer customer = new Customer(1);
+    Customer customer = new Customer(TEST_CUSTOMER_ID);
     customer.setLastName(TEST_CUSTOMER_LAST_NAME);
     customer.update();
     db.commit();
@@ -71,7 +98,7 @@ class SqlFunctionTest extends AbstractPrideTest {
   @Test
   void testFunction_ltrim() throws SQLException {
     Database db = DatabaseFactory.getDatabase();
-    Customer customer = new Customer(1);
+    Customer customer = new Customer(TEST_CUSTOMER_ID);
     customer.setLastName(TEST_CUSTOMER_LAST_NAME);
     customer.update();
     db.commit();
@@ -86,7 +113,7 @@ class SqlFunctionTest extends AbstractPrideTest {
   @Test
   void testFunction_rtrim() throws SQLException {
     Database db = DatabaseFactory.getDatabase();
-    Customer customer = new Customer(1);
+    Customer customer = new Customer(TEST_CUSTOMER_ID);
     customer.setLastName(TEST_CUSTOMER_LAST_NAME);
     customer.update();
     db.commit();
